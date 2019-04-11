99356bb8526cff7a4df7f5c5a078d2c4f347b942
Arlind Bardulla <echundu@gmail.com>
2019-04-11 23:24:25 +0200

    Add additional output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output zones_available {
   description = "Output gc zones having free space."
   value       = "{data.google_compute_zones.available.names}"
 }
+
+output instances_links {
+  description = "Output self_link of created instances."
+  value       = "${google_compute_instance.instance.*.self_link}"
+}
