685a2e8548ed8ba565d9b7f42cb0fb50f8c0b08c
Arlind Bardulla <echundu@gmail.com>
2019-04-21 15:31:45 +0200

    Add instances_tags output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output zones_available {
+output "zones_available" {
   description = "Output gc zones having free space."
   value       = "{data.google_compute_zones.available.names}"
 }
 
-output instances_self_links {
+output "instances_self_links" {
   description = "Output self_link of created instances."
   value       = "${google_compute_instance.instances.*.self_link}"
+}
+
+output "instances_tags" {
+  description = "Output tags of created instances."
+  value       = "${google_compute_instance.instances.*.tags}"
 }
